61fb870d9b0ab9ae2fa7d4f0edf09cfbdf8add9c
darwinestefano <darwinstefano12@hotmail.com>
2020-12-28 01:32:17 +0000

    Milestone 5

--- app/src/main/java/com/example/minesweeper/GameView.java
+++ app/src/main/java/com/example/minesweeper/GameView.java
@@ -10,11 +10,12 @@ import android.widget.Toast;
 public class GameView {
 
     private static GameView instance;
-    public static final int BOMB_NUMBER = 20;
+    public static final int BOMB_NUMBER = 10;
     public static final int WIDTH = 10;
     public static final int HEIGHT = 10;
 
     private Context context;
+
     private Cell[][] MinesweeperGrid = new Cell[WIDTH][HEIGHT];
 
     public static GameView getInstance() {
@@ -33,7 +34,19 @@ public class GameView {
         // create the grid and store it
         int[][] GeneratedGrid = Provider.provide(BOMB_NUMBER,WIDTH, HEIGHT);
         Board.print(GeneratedGrid,WIDTH,HEIGHT);
+        setGrid(context,GeneratedGrid);
+    }
 
+    private void setGrid( final Context context, final int[][] grid ){
+        for( int x = 0 ; x < WIDTH ; x++ ){
+            for( int y = 0 ; y < HEIGHT ; y++ ){
+                if( MinesweeperGrid[x][y] == null ){
+                    MinesweeperGrid[x][y] = new Cell( context , x,y);
+                }
+                MinesweeperGrid[x][y].setValue(grid[x][y]);
+                MinesweeperGrid[x][y].invalidate();
+            }
+        }
     }
 
     public Cell getCellAt(int position) {
@@ -60,13 +73,57 @@ public class GameView {
                     }
                 }
             }
+            //if bomb is revealed user lost game
+            if( getCellAt(x,y).isBomb()){
+                gameOver();
 
+            }
         }
+        //Check if user won game
+        checkWinner();
+    }
+
+    private boolean checkWinner(){
+        int bombNotFound = BOMB_NUMBER;
+        int notRevealed = WIDTH * HEIGHT;
+        for ( int x = 0 ; x < WIDTH ; x++ ){
+            for( int y = 0 ; y < HEIGHT ; y++ ){
+                if( getCellAt(x,y).isRevealed() || getCellAt(x,y).isFlagged() ){
+                    notRevealed--;
+                }
+
+                if( getCellAt(x,y).isFlagged() && getCellAt(x,y).isBomb() ){
+                    bombNotFound--;
+                }
+            }
+        }
+
+        if( bombNotFound == 0 && notRevealed == 0 ){
+            Toast.makeText(context,"Game won", Toast.LENGTH_SHORT).show();
+        }
+        return false;
     }
 
     public void flag( int x , int y ){
         boolean isFlagged = getCellAt(x,y).isFlagged();
         getCellAt(x,y).setFlagged(!isFlagged);
         getCellAt(x,y).invalidate();
+    }
+
+    //Game Over  --> Deals with end of the game, user can choose restart game
+    private void gameOver(){
+        // handle lost game
+        AlertDialog dialog = new AlertDialog.Builder(context)
+                .setTitle("GAME OVER")
+                .setMessage("Reset game to start over!")
+                .setNegativeButton("NO", null)
+                .setPositiveButton("RESET", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int i) {
+                        GameView.getInstance().createGrid(context);
+                    }
+                }).create();
+        dialog.show();
+
     }
 }
--- app/src/main/java/com/example/minesweeper/MainActivity.java
+++ app/src/main/java/com/example/minesweeper/MainActivity.java
@@ -3,12 +3,46 @@ package com.example.minesweeper;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    //Create btn
+    private Button resetBtn, coverBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Get a game view and display it
+        GameView.getInstance().createGrid(this);
+
+        //Reset game and generate a new instance of Grid
+        resetBtn = findViewById(R.id.reset);
+        resetBtn.setOnClickListener((v) -> {
+            GameView.getInstance().createGrid(this);
+        });
+
+        //Uncover Mine Sweeper game
+        coverBtn = findViewById(R.id.uncover);
+        coverBtn.setTag(1);
+        coverBtn.setText("UNCOVER MODE");
+        coverBtn.setOnClickListener( new View.OnClickListener() {
+            @Override
+            public void onClick (View v) {
+                final int status =(Integer) v.getTag();
+                if(status == 1) {
+                    //mPlayer.start();
+                    coverBtn.setText("MARKING MODE");
+                    v.setTag(0); //pause
+                } else {
+                    coverBtn.setText("UNCOVER MODE");
+                    v.setTag(1); //pause
+                }
+            }
+        });
     }
+
 }
